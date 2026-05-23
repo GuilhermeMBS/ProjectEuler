aeb2ea46ab83526a06db6cca21187da0cfa0e1ac
Guilherme <guiguimbs@hotmail.com>
2026-05-23 15:56:30 -0300

    First Problem Solution

--- ProblemOne/ProblemOne.cpp
+++ ProblemOne/ProblemOne.cpp
@@ -14,39 +14,20 @@ typedef set<ll> s64;
 #define ln "\n"
 
 
-void print_u128(unsigned __int128 n) {
-    if (n == 0) {
-        printf("0");
-        return;
-    }
-    char str[40]; // 128-bit max is ~3.4e38, so 40 chars is safe
-    int i = 0;
-    while (n > 0) {
-        str[i++] = (n % 10) + '0';
-        n /= 10;
-    }
-    while (--i >= 0) putchar(str[i]);
-}
-
-
-void print_s128(__int128 n) {
-    if (n < 0) {
-        printf("-");
-        n = -n;
-    }
-    print_u128((unsigned __int128)n);
+ll sumPA(ll a1, ll n, ll r) {
+    ll an = (a1 + (n-1)*r);
+    return (a1 + an)*n/2;
 }
 
 
 int main() {
     fast_cin();
-    
-    u128 n = 311499;
-    u128 s1 = (n*(n+1)*(2*n+1))/6;
-    u128 s2 = (n*(n+1))/2;
-    u128 s3 = n + 1;
+    ll m3 = sumPA(3, 999/3, 3);
+    ll m5 = sumPA(5, 999/5, 5);
+    ll m15 = sumPA(15, 999/15, 15);
 
-    u128 ans = 4*s1 + 4*s2 + s3;
-    print_s128(ans);
+    ll ans = m3 + m5 - m15;
+    printf("M3 = %llu\nM5 = %llu\nM15 = %llu\nANS = %llu", m3, m5, m15, ans);
+
     return 0;
 }
